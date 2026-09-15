eca9d43c4e67bcdc059bf99360238d7608c5a056
Abhijit Karale <abhijitkarale8@gmail.com>
2026-09-15 21:47:22 +0530

    Update uart_top.sv

--- rtl/uart_top.sv
+++ rtl/uart_top.sv
@@ -1,4 +1,4 @@
-// ============================================================================
+ // ============================================================================
 // Module Name  : uart_top
 // Description  : Top-level UART Controller module encapsulating Baud Rate 
 //                Generator, Transmitter, and Receiver with flexible configuration.
